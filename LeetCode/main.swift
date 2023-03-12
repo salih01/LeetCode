bd7a635fde9d8e8b4cc7699b8825dbe1eedd192f
Salih Cakmak <slhckmk01@gmail.com>
2023-03-13 00:10:06 +0300

    Running Sum of 1d Array

--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -77,3 +77,63 @@ var obj = Polindrome()
 print(obj.isPolindrome(232))
 
  */
+
+// **********************************************
+// ---------------- 2)Roman To Integer ----------------
+// **********************************************
+
+/*
+class RomanToInteger {
+    
+    let dictionary:[Character:Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
+
+    func romanToInt(_ s:String) -> Int {
+        var prev = 0, out = 0
+        for i in s {
+            
+            let value = dictionary[i] ?? 0
+            out += value <= prev ? prev : -prev
+            prev = value
+        }
+        out += prev
+     
+        print(out)
+        return out
+        
+    }
+    
+}
+
+
+var obj = RomanToInteger()
+obj.romanToInt("M")
+
+
+*/
+
+
+
+// **********************************************
+// ------- 1480. Running Sum of 1d Array ------
+// **********************************************
+
+class OneDArray{
+    
+    
+    func runningSum(_ nums:[Int]) -> [Int] {
+        
+        var dizi = nums
+        
+        for i in 1..<nums.count {
+            dizi[i] = nums[i] + dizi[i-1]
+        
+        }
+        return dizi
+    }
+    
+}
+
+
+var oneArray = OneDArray()
+oneArray.runningSum([3,4,5])
+
